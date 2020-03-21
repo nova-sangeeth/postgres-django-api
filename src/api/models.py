4bef858fd537724a140d26ff3fabe3c30d639e3d
nova-sangeeth <novasangeeth@outlook.com>
2020-03-21 19:11:16 +0530

    initial commit, models and applications created as required.

---
+++ src/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class userinfo(models.Model):
+    name = models.CharField(max_length=256, null=True)
+    user_code = models.CharField(max_length=256, null=True)
+    phone_number = models.CharField(max_length=256, null=True)
+    location = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return self.name
